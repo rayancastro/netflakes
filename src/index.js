f616a51de6089bd37df3d1d1a2bb14416f84553d
rayancastro <rayancdc@gmail.com>
2019-03-09 14:41:39 -0300

    Add redux, axios and bootstrap packages and configure index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Router from "./Router";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
+
+//Add Bootstrap module
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+  	<Router />
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
